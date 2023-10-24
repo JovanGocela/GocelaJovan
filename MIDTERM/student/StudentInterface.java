4570c8535975f0b20f1e6662365f881243fe943e
JovanGocela <148180224+JovanGocela@users.noreply.github.com>
2023-10-24 13:04:20 +0800

    Add files via upload

---
+++ MIDTERM/student/StudentInterface.java
@@ -0,0 +1,19 @@
+package Student;
+
+class StudentInterface {
+
+ public static void main (String a[]) {
+       Student sOne = new Student();
+       s0ne.setFirstName("Jovan");
+       sOne.setMiddleName("Temple");
+       s0ne.setLastName("Gocela");
+       sOne.setSuffix("Jr.");
+       
+       System.out.println("Student Full Name: " sOne.getFullName());
+       
+      }
+    }
+       
+       
+ 
+ 
